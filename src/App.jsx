c8cc90a52b218605299ef87294c719009f292b75
tawiah <tawiahin4k@gmail.com>
2024-12-19 14:31:44 +0000

    refactor: update ESLint config, add new dependencies, and improve component structure
    - Disable prop-types rule in ESLint
    - Add react-just-parallax and scroll-lock dependencies
    - Rename HambugerMenu to HamburgerMenu for consistency
    - Add onClick handler to Button component
    - Introduce Generating, CompanyLogos, Section, and Notification components
    - Update App component to include Hero and adjust imports
    - Enhance Header component with navigation toggle functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 /** @format */
 
-import ButtonGradinet from './assets/svg/ButtonGradient';
-import Header from './components/Header';
+import ButtonGradinet from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
-const App = ()=> {
+const App = () => {
   return (
     <>
-      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
+
       <ButtonGradinet />
     </>
-  )
-}
+  );
+};
 
 export default App;
