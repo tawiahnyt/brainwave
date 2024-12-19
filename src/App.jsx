1c27762c5d0740c356cde305f4f4c08615d4aad3
tawiah <tawiahin4k@gmail.com>
2024-12-19 10:37:20 +0000

    initialize project with Vite, React, and Tailwind CSS setup; add essential components and assets

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import ButtonGradinet from './assets/svg/ButtonGradient';
+import Header from './components/Header';
+
+const App = ()=> {
+  return (
+    <>
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+        <Header />
+      </div>
+      <ButtonGradinet />
+    </>
+  )
+}
+
+export default App;
